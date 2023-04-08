ae39294e1ad145d27504b63d0b7fd070154b4bf3
Aniket Mishrikotkar <71699313+aniket-mish@users.noreply.github.com>
2023-04-08 13:07:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import wandb
 print(f"The wandb version is {wandb.__version__}")
 
 # pytest
-assert (wandb.__version__ == '2.12.3', f'Expected version is {wandb.__version__} and we got '2.12.3'')
+assert (wandb.__version__ == '2.12.3', f"Expected version is {wandb.__version__} and we got '2.12.3'")
